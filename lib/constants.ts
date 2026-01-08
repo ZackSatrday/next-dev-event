5c07a69c33d38ae090de2383a1b5d562679a9dbb
ZackSatrday <sushant.kr398@gmail.com>
2026-01-08 22:44:10 +0530

    first commit

---
+++ lib/constants.ts
@@ -0,0 +1,50 @@
+export const events = [
+  {
+    title: "React Conf 2024",
+    image: "/images/event1.png",
+    slug: "react-conf-2024",
+    location: "Las Vegas, NV",
+    date: "May 15-17, 2024",
+    time: "9:00 AM - 6:00 PM"
+  },
+  {
+    title: "Vue.js London",
+    image: "/images/event2.png",
+    slug: "vue-js-london",
+    location: "London, UK",
+    date: "October 10-11, 2024",
+    time: "10:00 AM - 5:00 PM"
+  },
+  {
+    title: "Angular Connect",
+    image: "/images/event3.png",
+    slug: "angular-connect",
+    location: "London, UK",
+    date: "September 25-26, 2024",
+    time: "9:00 AM - 6:00 PM"
+  },
+  {
+    title: "Hack the North",
+    image: "/images/event4.png",
+    slug: "hack-the-north",
+    location: "Waterloo, ON",
+    date: "September 14-15, 2024",
+    time: "8:00 AM - 8:00 PM"
+  },
+  {
+    title: "JSConf EU",
+    image: "/images/event5.png",
+    slug: "jsconf-eu",
+    location: "Berlin, Germany",
+    date: "June 12-14, 2024",
+    time: "9:00 AM - 6:00 PM"
+  },
+  {
+    title: "TechCrunch Disrupt",
+    image: "/images/event6.png",
+    slug: "techcrunch-disrupt",
+    location: "San Francisco, CA",
+    date: "October 28-30, 2024",
+    time: "8:00 AM - 6:00 PM"
+  }
+];
